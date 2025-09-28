741fe059164d23516fba872688fa3642d6928a63
chaninduisuranga <159243993+chaninduisuranga@users.noreply.github.com>
2025-09-28 12:17:20 +0530

    page

--- service/sensorApi.ts
+++ service/sensorApi.ts
@@ -1,14 +1,16 @@
-import {apiResponse} from "@/types/common";
+import { apiResponse } from "@/types/common";
 import privateAxios from "@/lib/privateAxios";
 import ToastUtils from "@/utils/toastUtils";
-import {CreateSensorFormData} from "@/types/sensors/admin";
+import { CreateSensorFormData, Sensor } from "@/types/sensors/admin";
 
-export const createSensor = async (data: CreateSensorFormData): Promise<apiResponse | null> => {
-    try {
-        await privateAxios.post<void>("/sensors", data);
-        return {success: true};
-    } catch (error: any) {
-        ToastUtils.error("Sensor creation failed. " + error?.response.data.message);
-        return {success: false, error: error.response.data.message};
-    }
+export const createSensor = async (
+  data: CreateSensorFormData
+): Promise<apiResponse | null> => {
+  try {
+    await privateAxios.post<void>("/sensors", data);
+    return { success: true };
+  } catch (error: any) {
+    ToastUtils.error("Sensor creation failed. " + error?.response.data.message);
+    return { success: false, error: error.response.data.message };
+  }
 };
