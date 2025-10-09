a46c6eebc81eeb5ffdb3305cbe30fb3af75cc514
Hasindu Chanuka <148543372+hasindu1998@users.noreply.github.com>
2025-10-09 23:33:04 +0530

    Implement the user management section in admin dashboard

---
+++ app/admin/create-user/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CreateUser from "@/components/admin/create-user/CreateUser";
+
+export default function CreateUserPage() {
+  return (
+    <div className="px-3 md:px-4 mx-auto w-full -m-15 pt-1 pb-7">
+      <CreateUser />
+    </div>
+  );
+}
